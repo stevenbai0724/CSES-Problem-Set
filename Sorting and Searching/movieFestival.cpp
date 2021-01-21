4fee1b7ad2a3ebf7f59dc7cd821807b49507c1cf
Steven Bai <51672429+stevenbai0724@users.noreply.github.com>
2021-01-21 10:39:44 -0500

    Add files via upload

--- Sorting and Searching/movieFestival.cpp
+++ Sorting and Searching/movieFestival.cpp
@@ -1,32 +1,27 @@
-/*
-https://cses.fi/problemset/task/1629
-
--sort in increasing order by the ending time
-
-*/
+//https://cses.fi/problemset/task/1629
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
     cin.tie(nullptr)->sync_with_stdio(false);
 
     int n; cin>>n;
-    vector<pair<int, int>>arr(n+1);
-
-    for(int i=1;i<=n;i++){
+    vector<pair<int, int>>arr(n);
+    for(int i=0;i<n;i++){
         cin>>arr[i].second>>arr[i].first;
     }
     sort(arr.begin(), arr.end());
-    int cur = 0;
-    int count = 0;
-
-    for(int i=1;i<=n;i++){
-        if(arr[i].second>=cur){
-            count++;
-            cur = arr[i].first;
+    int cur = 0;    
+    int ans = 0;
+    for(auto p: arr){
+        int start = p.second;
+        int end = p.first;
+        if(start>=cur){
+            cur = end;
+            ans++;
         }
     }
-    cout<<count;
-    
+    cout<<ans<<"\n";
+
 
     return 0;
 }
