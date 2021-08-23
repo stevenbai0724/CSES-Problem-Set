6319f7f3dd5b0284ac5d9f9bfc0ed6651d9a335c
stevenbai0724 <stevenbai0724@gmail.com>
2021-08-23 12:39:12 -0400

    8/23/21

--- Sorting and Searching/stickLengths.cpp
+++ Sorting and Searching/stickLengths.cpp
@@ -6,16 +6,29 @@ signed main(){
     cin.tie(nullptr)->sync_with_stdio(false);
 
     int n; cin>>n;
+    
     vector<int>arr(n+1);
     for(int i=1;i<=n;i++){
         cin>>arr[i];
     }
-    int count = 0;
+    if(n==1){
+        cout<<0<<"\n";
+        return 0;
+    }
+    if(n==2){
+        cout<<abs(arr[1]-arr[2])<<"\n";
+        return 0;
+    }
+    int count1 = 0;
+    int count2 = 0;
+    int count3 = 0;
     sort(arr.begin(), arr.end());
     for(int i=1;i<=n;i++){
-        count+=abs(arr[i]-arr[n/2]);
+        count1+=abs(arr[i]-arr[n/2]);
+        count2+=abs(arr[i]-arr[n/2+1]);
+        count3+=abs(arr[i]-arr[n/2-1]);
     }
-    cout<<count;
+    cout<<min(count1, min(count2, count3))<<"\n";
 
 
     return 0;
